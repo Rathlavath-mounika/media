5d73728281e0f4ff04b18ccb657824364c931dff
MOUNIKA <mounikasomla12@gmail.com>
2025-03-22 15:35:26 +0530

    Add files via upload

---
+++ day4.py
@@ -0,0 +1,21 @@
+Month = 6
+
+match Month:
+    case 1:
+        print("january")
+    case 2:
+        print("febraury")
+    case 3:
+        print("march")
+    case 4:
+        print("april")
+    case 5:
+        print("may")
+    case 6:
+        print("june")
+    case 7:
+        print("july")
+    case 8:
+        print("august")
+    case _:
+        print("invalid month")
