cb0c46a45107f39b15c47548d90bda44508708b3
MOUNIKA <mounikasomla12@gmail.com>
2025-03-22 15:53:45 +0530

    Add files via upload

---
+++ day6.py
@@ -0,0 +1,23 @@
+#IDENTITY OPERATOR
+a=[1,2,3]
+b=a
+c=[1,2,3]
+print(a is b)
+print(a is c)
+print(a is not c)
+
+#Comparision operator
+x=5
+y=10
+print(x==y)
+print(x!=y)
+print(x>y)
+print(x<y)
+print(x>=y)
+print(x<=y)
+
+#Membership operator
+fruits=["apple","banana","cherry"]
+print("apple" in fruits)
+print("grape" not in fruits)
+
