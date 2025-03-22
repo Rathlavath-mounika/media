c44c946e31ac0f90f61ea1b5c75704355c3bff99
MOUNIKA <mounikasomla12@gmail.com>
2025-03-22 15:48:42 +0530

    Add files via upload

---
+++ day5.py
@@ -0,0 +1,61 @@
+#logical operator
+#AND
+
+a=(21 and 56)
+print(a)
+
+b=(56 and 21)
+print(b)
+
+c=(56 and 56)
+print(c)
+
+d=(21 and 21)
+print(d)
+
+#OR 
+a1=(21or 56)
+print(a1)
+
+a2=(56 or 21)
+print(a2)
+
+a3=(56 or 56)
+print(a3)
+
+a4=(21 or 21)
+print(a4)
+
+#NOT
+a=(not (not 3))
+print(a)
+
+b=(not 3)
+print(b)
+
+#BITWISE OPERATOR
+#&
+a=2
+b=3
+print(a&b)
+
+#|
+a=2
+b=3
+print(a|b)
+
+#~
+a=(~-3)
+print(a)
+
+# Bitwise left shift(<<)
+a=(4<<3)
+print(a)
+
+#Bitwise right shift(>>)
+a=(4>>2)
+print(a)
+
+
+
+
