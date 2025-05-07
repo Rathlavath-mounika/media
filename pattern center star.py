984baa347a63de89f54f89d63fb7b9a85928d438
MOUNIKA <mounikasomla12@gmail.com>
2025-05-07 11:23:53 +0530

    Add files via upload

--- pattern center star.py
+++ pattern center star.py
@@ -1,9 +1,11 @@
-size=5
-center =size //2
-for i in range(size):
-    for j in range(size):
-        if i==center and j==center:
-            print("*",end="")
+rows=5
+mid=rows//2 + 1
+for i in range(1,rows+1):
+    res=""
+    for j in range(1,rows+1):
+        if i==1 or i==rows or j==1 or j==rows or i==mid and j==mid:
+          res+="*"+" "
         else:
-            print("",end="")
-    print()
+          res+=" "+" "
+    print(res)
+
